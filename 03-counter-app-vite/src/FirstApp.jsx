dbc910cea3ecabc39964c1564ef21183cda3a351
alonsobh <alonsobh@gmail.com>
2025-02-17 08:01:34 -0500

    03-counter-app-vite: simplify Fragment

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,11 +1,11 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 
 const FirstApp = () => {
   return (
-    <Fragment>
+    <>
         <h1>FirstApp</h1>
         <p>Soy un subtitulo</p>
-    </Fragment>
+    </>
   )
 }
 
