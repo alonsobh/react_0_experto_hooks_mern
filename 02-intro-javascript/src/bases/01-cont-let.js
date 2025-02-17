bac5f8f3ddb9d25654ae48c138d8a8d32f2c342f
alonsobh <alonsobh@gmail.com>
2025-02-16 21:23:53 -0500

    02-intro-javascript: 01-cont-let - bkp

---
+++ 02-intro-javascript/src/bases/01-cont-let.js
@@ -0,0 +1,21 @@
+
+// Variables y Constantes
+
+const nombre = 'Fernando';
+const apellido = 'Herrera';
+
+let valorDado = 5;
+
+console.log(nombre, apellido, valorDado);
+valorDado = 4;
+
+console.log(nombre, apellido, valorDado);
+
+if ( true ){
+    const nombre = 'Peter'
+    let valorDado = 6
+
+    console.log(nombre, apellido, valorDado);
+}
+
+console.log(nombre, apellido, valorDado);
