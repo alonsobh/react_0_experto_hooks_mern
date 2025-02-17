9783a1d78b4dee6109ce91bdd2c0cc3013800bfa
alonsobh <alonsobh@gmail.com>
2025-02-16 21:23:53 -0500

    02-intro-javascript: 01-cont-let - scope variables

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -10,3 +10,12 @@ console.log(nombre, apellido, valorDado);
 valorDado = 4;
 
 console.log(nombre, apellido, valorDado);
+
+if ( true ){
+    const nombre = 'Peter'
+    let valorDado = 6
+
+    console.log(nombre, apellido, valorDado);
+}
+
+console.log(nombre, apellido, valorDado);
